4b6c57c48ef5542f5d974ed2dedd65bf6cb54bc2
Jorge Atempa <atempaitm@gmail.com>
2023-01-14 22:41:30 -0800

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 require('dotenv').config();
 import express from 'express';
 import router from './router';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/api', router);
 
 app.listen(PORT, () => {
