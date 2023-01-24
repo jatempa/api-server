ab866a15b524f6eeb560a9d994c4d646f2ae4f61
Jorge Atempa <atempaitm@gmail.com>
2023-01-24 13:19:56 -0800

    Create methods to get information from db

---
+++ src/handlers/product.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import prisma from '../db';
+
+// Get all
+export const getProducts = async (req: Request, res: Response) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: req.user?.id,
+    },
+    include: {
+      products: true,
+    },
+  });
+
+  res.json({ data: user?.products });
+};
+
+// Get one
+export const getOneProduct = async (req: Request, res: Response) => {
+  const id = req.params.id;
+
+  const product = await prisma.product.findFirst({
+    where: {
+      id,
+      belongsToId: req.user?.id,
+    },
+  });
+
+  res.json({ data: product });
+};
