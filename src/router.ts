c94fb2541eada5179ecbe7f0bf557edbaa3f5c6d
Jorge Atempa <atempaitm@gmail.com>
2023-01-24 12:51:25 -0800

    Create 'middleware' to handle input errors

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { body, validationResult } from 'express-validator';
+import { handleInputErrors } from './modules/middleware';
 
 const router = Router();
 
@@ -8,14 +9,12 @@ const router = Router();
  */
 router.get('/product', (req, res) => {});
 router.get('/product/:id', (req, res) => {});
-router.put('/product/:id', body('name').isString(), (req, res) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    res.status(400);
-    res.json({ errors: errors.array() });
-  }
-});
+router.put(
+  '/product/:id',
+  body('name').isString(),
+  handleInputErrors,
+  (req, res) => {}
+);
 router.post('/product', (req, res) => {});
 router.delete('/product/:id', (req, res) => {});
 
